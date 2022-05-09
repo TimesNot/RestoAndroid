1ffac95f37fb5f52e756218bbaa9685d427600b9
TimesNot <86958577+TimesNot@users.noreply.github.com>
2022-05-09 21:05:49 +0200

    Add files via upload

---
+++ TestAndroid/DataBaseConfig.php
@@ -0,0 +1,18 @@
+<?php
+
+class DataBaseConfig{
+
+    public $servername;
+    public $username;
+    public $password;
+    public $databasename;
+
+    public function __construct()
+    {
+        $this->servername = 'localhost';
+        $this->username = 'u369603422_Admin';
+        $this->password = 'Admin.44200+';
+        $this->databasename = 'u369603422_bdd_Android';
+    }
+}
+?>
