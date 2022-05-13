e73e1c5ee5462e3b25091036aeecd74c70f89788
drouet djimy <tazzeurxx@gmail.com>
2022-05-13 14:11:32 +0200

    modification de l'affichage de la liste des restaurant

--- app/src/main/java/com/example/restoandroid/adapters/Restaurant_adapter.java
+++ app/src/main/java/com/example/restoandroid/adapters/Restaurant_adapter.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.TextView;
 
 import com.example.restoandroid.ListeRestaurant;
 import com.example.restoandroid.R;
@@ -44,6 +45,19 @@ public class Restaurant_adapter extends BaseAdapter {
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
         view = inflater.inflate(R.layout.adapter_restaurant,null);
+
+        //information a propos du restaurant
+        Restaurant currentRestaurant = getItem(i);
+        String restaurantNom = currentRestaurant.getNom();
+        String restaurantVille = currentRestaurant.getVille();
+
+        // affiche restaurant nom view
+        TextView restaurantNomView = view.findViewById(R.id.restaurant_nom);
+        restaurantNomView.setText(restaurantNom);
+
+        // affiche restaurant ville view
+        TextView restaurantVilleView = view.findViewById(R.id.restaurant_ville);
+        restaurantVilleView.setText(restaurantVille);
         return view;
     }
 }
